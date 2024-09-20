0176ef9f83a73dd8ac95dfaa3cc8e653755ddd1f
Sujal Gupta <sujalgupta6100@gmail.com>
2024-09-20 21:41:10 +0530

    add Users
    Signed-off-by: Sujal Gupta <sujalgupta6100@gmail.com>

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,39 +1,35 @@
-import useFetchUsers from "../../../hooks/useFetchUsers";
-import ShimmerDisplay from "../ShimmerDisplay/ShimmerDisplay";
 import UserCard from "../UserCard/UserCard";
 
 // This component simply renders all the users
-const Users = ({ pageNumber }) => {
-  const { users, loading, error } = useFetchUsers(pageNumber);
-  console.log(users);
-  if(loading){
-    return <ShimmerDisplay/>
-  }
-  if(error){
-    return <h1 className="text-center">Oops!, {error} </h1>
-  }
-  return <div className="cards-container flex flex-wrap justify-around">
- {   users?.map((user) => {
-      return (
-        <UserCard
-          
-          key={user.ID}
-          id={user.ID}
-          jobTitle={user.JobTitle}
-          emailAddress={user.EmailAddress}
-          fullName={user.FirstNameLastName}
-          email={user.Email}
-          phone={user.Phone}
-          company={user.Company}
-        />
-      );
-    })
-    }
+interface User {
+  ID: string;
+  JobTitle: string;
+  EmailAddress: string;
+  FirstNameLastName: string;
+  Email: string;
+  Phone: string;
+  Company: string;
+}
 
-  </div>
-  
+const Users = ({ users }: { users: User[] }) => {
+  return (
+    <div className="cards-container flex flex-wrap justify-around">
+      {users?.map((user: User) => {
+        return (
+          <UserCard
+            key={user.ID}
+            id={user.ID}
+            jobTitle={user.JobTitle}
+            emailAddress={user.EmailAddress}
+            fullName={user.FirstNameLastName}
+            email={user.Email}
+            phone={user.Phone}
+            company={user.Company}
+          />
+        );
+      })}
+    </div>
+  );
 };
 
 export default Users;
-
- 
